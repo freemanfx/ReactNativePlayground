8bcde33196ee45356872433570214a447f9fad43
freemanfx <freemanfx@gmail.com>
2017-01-05 18:01:24 +0200

    really buggy navigator

--- app/mainscreen.js
+++ app/mainscreen.js
@@ -1,14 +1,54 @@
 import React, {Component} from "react";
-import {AppRegistry, StyleSheet, Text, View} from "react-native";
+import {AppRegistry, StyleSheet, Text, View, Navigator} from "react-native";
 
 export default class MainScreen extends Component {
-    render() {
+
+    renderScene(route, navigator) {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native! {this.props.name}
-                </Text>
-            </View>
+            <Text style={styles.text}>{route.title}</Text>
+        );
+    }
+
+    render() {
+        const routes = [
+            {title: 'First', index: 0},
+            {title: 'Second', index: 1},
+            {title: 'Third', index: 2},
+        ];
+
+        return (
+            <Navigator
+                initialRoute={routes[0]}
+                initialRouteStack={routes}
+                renderScene={(route,navigator) => this.renderScene(route,navigator)}
+                navigationBar={
+                           <Navigator.NavigationBar
+                            routeMapper={{
+                                    LeftButton: (route,navigator, index, navState) => {
+                                        if(index == 0){
+                                            return null;
+                                        } else {
+                                            return (<Text style={styles.navButton} onPress={() => navigator.pop()}>Back</Text>);
+                                        }
+                                    },
+                                    RightButton: (route, navigator, index, navState) => {
+                                        if(index == routes.length -1) {
+                                            return null;
+                                        }
+                                        return (<Text onPress={() => navigator.push(routes[index])}
+                                                    style={styles.navButton}>
+                                                    Next
+                                                </Text>);
+                                    },
+                                    Title: (route, navigator, index, navState) =>{
+                                        return (<Text style={styles.navTitle}>{route.title}</Text>);
+                                    },
+                                 }}
+                            />
+                       }
+            >
+
+            </Navigator>
         );
     }
 }
@@ -20,10 +60,19 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'white',
     },
-    welcome: {
+    text: {
         fontSize: 20,
         textAlign: 'center',
-        margin: 10,
-        color: 'black'
+        color: 'black',
+        marginTop: 100
+    },
+    navButton: {
+        padding: 10,
+        fontWeight: 'bold'
+    },
+    navTitle: {
+        padding: 10,
+        fontWeight: 'bold',
+        textAlign: 'center'
     }
 });
